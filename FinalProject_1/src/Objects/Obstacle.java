b1f951c45635eae418b646aba44dace4f028c8c5
2246982 <123512815+2246982@users.noreply.github.com>
2023-12-01 13:50:18 -0500

    Add files via upload

---
+++ FinalProject_1/src/Objects/Obstacle.java
@@ -0,0 +1,18 @@
+package Objects;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 2246982
+ */
+public abstract class Obstacle extends Body {
+    boolean isbreakable;
+    double Strength;
+
+    
+    
+}
